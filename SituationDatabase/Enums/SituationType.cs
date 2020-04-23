07654182d13075746ce12b2374f3ef77f1565489
Kieron <kieron@mentor.gg>
2020-04-23 18:32:46 +0200

    Add Situation related interfaces and enum

---
+++ SituationDatabase/Enums/SituationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SituationDatabase.Enums
+{
+    /// <summary>
+    /// A type of situation.
+    /// </summary>
+    public enum SituationType : int
+    {
+        Misplay = 1,
+        Highlight = 2,
+    }
+}
---
+++ SituationOperator/Interfaces/IAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SituationOperator
+{
+    /// <summary>
+    /// An [Event/Action] carried out by one or more players, starting at a particular time.
+    /// </summary>
+    public interface IAction
+    {
+    }
+}
