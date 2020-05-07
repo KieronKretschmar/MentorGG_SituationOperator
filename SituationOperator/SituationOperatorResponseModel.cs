4050aeafc90100725563b3d8807c2228ae0bd679
Kieron <kieron@mentor.gg>
2020-05-07 10:14:52 +0200

    Add Placeholder for amqp response model

---
+++ SituationOperator/SituationOperatorResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using RabbitCommunicationLib.TransferModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SituationOperator
+{
+    public class SituationOperatorResponseModel : TransferModel
+    {
+    }
+}
