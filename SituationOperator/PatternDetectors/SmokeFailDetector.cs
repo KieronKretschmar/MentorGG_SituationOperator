156ded205406ca0858f97e56e7361ba6283f9af2
Kieron <kieron@mentor.gg>
2020-05-08 16:07:00 +0200

    Implement SmokeFailDetector and -Manager

--- SituationOperator/PatternDetectors/SmokeFailDetector.cs
+++ SituationOperator/PatternDetectors/SmokeFailDetector.cs
@@ -1,26 +1,58 @@
 ﻿using MatchEntities;
+using Microsoft.Extensions.Logging;
 using SituationDatabase;
 using SituationDatabase.Models;
 using SituationOperator.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZoneReader;
 
 namespace SituationOperator.PatternDetectors
 {
     public class SmokeFailDetector : IPatternDetector<SmokeFail>
     {
-        public Task<IEnumerable<SmokeFail>> ExtractSituations(MatchDataSet matchData)
+        private readonly ILogger<SmokeFailDetector> _logger;
+        private readonly IZoneReader _zoneReader;
+
+        public SmokeFailDetector(
+            ILogger<SmokeFailDetector> logger,
+            IZoneReader zoneReader)
         {
-            var myList = new List<SmokeFail>
+            _logger = logger;
+            _zoneReader = zoneReader;
+        }
+
+        public async Task<IEnumerable<SmokeFail>> ExtractSituations(MatchDataSet matchData)
+        {
+            var map = matchData.MatchStats.Map;
+
+            // If the map is not known, return empty collection
+            if (!Enum.TryParse(map, true, out ZoneReader.Enums.Map mapFromEnum))
             {
-                new SmokeFail
+                _logger.LogDebug($"No zones are defined for map {map} and match {matchData.MatchId}, returning empty collection");
+                return new List<SmokeFail>();
+            }
+
+            var lineups = _zoneReader.GetLineups(ZoneReader.Enums.LineupType.Smoke, mapFromEnum).Lineups;
+
+            var failedSmokes = matchData.SmokeList
+                .Where(x => x.Result == MatchEntities.Enums.TargetResult.Miss)
+                .Select(x => new SmokeFail
                 {
-                    MatchId = 1,
-                    LineupId = 1
-                }
-            };
-            return Task.FromResult(myList.AsEnumerable());
+                    MatchId = x.MatchId,
+                    Map = x.MatchStats.Map,
+                    MatchDate = x.MatchStats.MatchDate,
+                    Round = x.Round,
+                    StartTime = x.Time,
+                    PlayerId = x.PlayerId,
+                    LineupId = x.LineUp,
+                    LineupName = lineups[x.LineUp].Name
+                })
+                .ToList();
+
+            return failedSmokes;
         }
     }
 }
--- SituationOperator/SituationManagers/SmokeFailManager.cs
+++ SituationOperator/SituationManagers/SmokeFailManager.cs
@@ -12,9 +12,16 @@ namespace SituationOperator.SituationManagers
 {
     public class SmokeFailManager : SituationManager<SmokeFail>
     {
+        private readonly SmokeFailDetector _smokeFailDetector;
+
+        public SmokeFailManager(SmokeFailDetector smokeFailDetector)
+        {
+            _smokeFailDetector = smokeFailDetector;
+        }
+
         public override SituationCategory SituationCategory => SituationCategory.Misplay;
 
-        protected override IPatternDetector<SmokeFail> Detector => new SmokeFailDetector();
+        protected override IPatternDetector<SmokeFail> Detector => _smokeFailDetector;
 
         protected override Func<SituationContext, DbSet<SmokeFail>> TableSelector => context => context.SmokeFail;
     }
