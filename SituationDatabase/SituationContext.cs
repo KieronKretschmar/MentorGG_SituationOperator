72f7033261b37d24544a23481f2f968f2f7b86db
Kieron <kieron@mentor.gg>
2020-05-08 16:52:32 +0200

    Add ModelBuilderExtensions to improve code structure in SituationContext.cs

--- SituationDatabase/SituationContext.cs
+++ SituationDatabase/SituationContext.cs
@@ -22,8 +22,22 @@ namespace SituationDatabase
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region Misplays - SinglePlayer
+            modelBuilder.AddSinglePlayerSituation<SmokeFail>();
+            #endregion
 
-            modelBuilder.Entity<SmokeFail>(entity =>
+            #region Goodplays - SinglePlayer
+            modelBuilder.AddSinglePlayerSituation<EffectiveHeGrenade>();
+            #endregion
+        }
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        public static void AddSituation<TSituation>(this ModelBuilder modelBuilder)
+            where TSituation : class, ISituation
+        {
+            modelBuilder.Entity<TSituation>(entity =>
             {
                 entity.HasKey(e => new { e.MatchId, e.Id });
 
@@ -31,12 +45,18 @@ namespace SituationDatabase
 
                 entity.Property(x => x.Id).ValueGeneratedOnAdd();
             });
+        }
 
-            modelBuilder.Entity<EffectiveHeGrenade>(entity =>
+        public static void AddSinglePlayerSituation<TSituation>(this ModelBuilder modelBuilder)
+            where TSituation : class, ISinglePlayerAction
+        {
+            modelBuilder.Entity<TSituation>(entity =>
             {
                 entity.HasKey(e => new { e.MatchId, e.Id });
 
                 entity.HasIndex(e => e.MatchId);
+
+                entity.HasIndex(e => e.SteamId);
 
                 entity.Property(x => x.Id).ValueGeneratedOnAdd();
             });
