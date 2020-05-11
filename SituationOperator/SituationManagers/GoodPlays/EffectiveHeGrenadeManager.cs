6e21ee6865989a30a85ab39d3eac7f418a5c87c0
Kieron <kieron@mentor.gg>
2020-05-11 13:17:21 +0200

    Implement EffectiveHeGrenades

--- SituationOperator/SituationManagers/GoodPlays/EffectiveHeGrenadeManager.cs
+++ SituationOperator/SituationManagers/GoodPlays/EffectiveHeGrenadeManager.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using SituationDatabase;
 using SituationDatabase.Enums;
 using SituationDatabase.Models;
+using SituationOperator.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,8 @@ namespace SituationOperator.SituationManagers.GoodPlays
     /// </summary>
     public class EffectiveHeGrenadeManager : SituationManager<EffectiveHeGrenade>
     {
+        private const int MIN_TOTAL_DAMAGE = 50;
+
         private readonly IServiceProvider _sp;
         private readonly ILogger<EffectiveHeGrenadeManager> _logger;
 
@@ -36,13 +39,17 @@ namespace SituationOperator.SituationManagers.GoodPlays
         protected override Func<SituationContext, DbSet<EffectiveHeGrenade>> TableSelector => context => context.EffectiveHeGrenade;
 
         /// <summary>
-        /// 
+        /// Finds all HE grenades that were effective against enemies.
         /// </summary>
         /// <param name="data">Data of the match in which to look for situations for all players.</param>
         /// <returns></returns>
-        protected override Task<IEnumerable<EffectiveHeGrenade>> ExtractSituationsAsync(MatchDataSet data)
+        protected override async Task<IEnumerable<EffectiveHeGrenade>> ExtractSituationsAsync(MatchDataSet data)
         {
-            throw new NotImplementedException();
+            var hes = data.HeList
+                .Select(x=> new EffectiveHeGrenade(x, data.DamagesByHe(x)))
+                .Where(x => x.TotalEnemyDamage > MIN_TOTAL_DAMAGE);
+
+            return hes;
         }
     }
 }
