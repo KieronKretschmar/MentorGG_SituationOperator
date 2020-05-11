829c7517933cca461de483bddada02d8a7f78419
Kieron <kieron@mentor.gg>
2020-05-11 10:46:01 +0200

    Add MatchDataSetExtensions to make navigating without EFCore's navigational properties easier

---
+++ SituationOperator/Helpers/MatchDataSetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MatchEntities;
+using MatchEntities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SituationOperator.Helpers
+{
+    public static class MatchDataSetExtensions
+    {
+        /// <summary>
+        /// Gets the RoundStats object referenced by the given entity.
+        /// </summary>
+        /// <param name="matchData"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static RoundStats RoundStats(this MatchDataSet matchData, IRoundEntity entity)
+        {
+            return matchData.RoundStatsList.Single(x => x.Round == entity.Round);
+        }
+    }
+}
