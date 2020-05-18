2cf60dc9ef5d0eb7d10ed5ee0a6825c01b944100
Kieron <kieron@mentor.gg>
2020-05-18 12:28:30 +0200

    Add FlashExtensions

---
+++ SituationDatabase/Extensions/FlashExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MatchEntities;
+using SituationDatabase.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SituationDatabase.Extensions
+{
+    public static class FlashExtensions
+    {
+        /// <summary>
+        /// Upper bound for time a flash can have an effect on a single player.
+        /// 
+        /// See https://counterstrike.fandom.com/wiki/Flashbang
+        /// </summary>
+        public const int MAX_FLASH_TIME = 6000;
+    }
+}
