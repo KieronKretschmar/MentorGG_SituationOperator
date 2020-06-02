dfc895aeba2c647d185ab9f213c4844513ed5657
Kieron <kieron@mentor.gg>
2020-06-02 10:19:24 +0200

    Add SkillDomains enum

---
+++ SituationOperator/Enums/SkillDomain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SituationOperator.Enums
+{
+    /// <summary>
+    /// Each SituationType belongs to a SkillDomain, i.e. an area of expertise.
+    /// </summary>
+    public enum SkillDomain : byte
+    {
+        /// <summary>
+        /// The SkillDomain of not doing stupid shit.
+        /// </summary>
+        Tactical = 1,
+
+        /// <summary>
+        /// Skills related to grenade usage.
+        /// </summary>
+        Grenades = 2,
+
+        /// <summary>
+        /// Aiming, Fighting and killing.
+        /// </summary>
+        Shooting = 3,
+
+        /// <summary>
+        /// The art of pressing W,A,S,D.
+        /// </summary>
+        Movement = 4,
+    }
+}
