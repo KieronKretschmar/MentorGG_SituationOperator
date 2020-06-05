af4f69d6389a3395853fa9fb9fb9b763f26e79c8
Kieron <kieron@mentor.gg>
2020-06-05 17:50:59 +0200

    Add migration

--- SituationDatabase/Migrations/20200605155014_release-0.3.0.Designer.cs
+++ SituationDatabase/Migrations/20200605155014_release-0.3.0.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SituationDatabase;
 
 namespace SituationDatabase.Migrations
 {
     [DbContext(typeof(SituationContext))]
-    partial class SituationContextModelSnapshot : ModelSnapshot
+    [Migration("20200605155014_release-0.3.0")]
+    partial class release030
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
