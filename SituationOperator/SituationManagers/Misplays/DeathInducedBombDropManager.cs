6cb787a4eac71f4f8c9071858c6c01e1a97e38c8
Kieron <kieron@mentor.gg>
2020-06-10 14:32:10 +0200

    Change DeathInducedBombDrop to require 2 alive teammates and count teammates as alive who died shortly before

--- SituationOperator/SituationManagers/Misplays/DeathInducedBombDropManager.cs
+++ SituationOperator/SituationManagers/Misplays/DeathInducedBombDropManager.cs
@@ -25,12 +25,22 @@ namespace SituationOperator.SituationManagers
         /// <summary>
         /// Minimum required teammates alive at death to count as a misplay.
         /// </summary>
-        private const int MIN_TEAMMATES_ALIVE = 1;
+        private const int MIN_TEAMMATES_ALIVE = 2;
+
+        /// <summary>
+        /// Allowed time for a teammate to die before the bomb was dropped to count as alive.
+        /// </summary>
+        private const int MAX_TIME_TEAMMATE_DIED_BEFORE_TO_COUNT_AS_ALIVE = 4000;
 
         /// <summary>
         /// Minimum required distance in meters to the nearest teammate at death count as a misplay.
         /// </summary>
         private const double MIN_TEAMMATE_DISTANCE = 5.0;
+
+        /// <summary>
+        /// Minimum time the player must have held the bomb before dying to count as a misplay.
+        /// </summary>
+        private const double MIN_TIME_HELD_BOMB = 6000;
 
         private readonly IServiceProvider _sp;
         private readonly ILogger<DeathInducedBombDropManager> _logger;
@@ -70,10 +80,21 @@ namespace SituationOperator.SituationManagers
 
                 foreach (var bombDrop in bombDrops)
                 {
+                    var bombPickupBeforeDrop = data.ItemPickedUpList
+                        .Where(x => 
+                        x.Round == bombDrop.Round 
+                        && x.PlayerId == bombDrop.PlayerId 
+                        && x.Equipment == EquipmentElement.Bomb
+                        && x.Time <= bombDrop.Time)
+                        .OrderByDescending(x => x.Time)
+                        .FirstOrDefault();
+                    if (bombDrop.Time - MIN_TIME_HELD_BOMB < bombPickupBeforeDrop.Time)
+                        continue;
+
                     var livingTeammateSteamIds = data.GetTeammateRoundStats(bombDrop.PlayerId, bombDrop.Round)
                         .Select(x => x.PlayerId)
                         .Where(x => x != bombDrop.PlayerId)
-                        .Where(x => data.IsAlive(x, bombDrop.Round, bombDrop.Time));
+                        .Where(x => data.IsAlive(x, bombDrop.Round, bombDrop.Time - MAX_TIME_TEAMMATE_DIED_BEFORE_TO_COUNT_AS_ALIVE));
 
                     var teammatesAlive = livingTeammateSteamIds.Count();
 
