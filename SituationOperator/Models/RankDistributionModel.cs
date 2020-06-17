937fe9a735987551aa2f0313bf6e38c96d4914ce
Kieron <kieron@mentor.gg>
2020-06-17 16:34:46 +0200

    Add DistributionController to access datat for comparing occurences by rank

---
+++ SituationOperator/Models/RankDistributionModel.cs
@@ -0,0 +1,30 @@
+﻿using SituationDatabase;
+using SituationDatabase.Models;
+using SituationOperator.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SituationOperator.Models
+{
+    /// <summary>
+    /// Holds data about how <see cref="ISituation"/>s of different <see cref="SituationType"/> are distributed among different ranks.
+    /// </summary>
+    public class RankDistributionModel
+    {
+        /// <summary>
+        /// Dictionary with data for each <see cref="SituationType"/>.
+        /// </summary>
+        public Dictionary<SituationType, Dictionary<int, SituationInfoByRank>> Data { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RankDistributionModel()
+        {
+            Data = new Dictionary<SituationType, Dictionary<int, SituationInfoByRank>>();
+        }
+
+    }
+}
