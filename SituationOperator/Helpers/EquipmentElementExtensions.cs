da3bc8c8e5ed025add291c2d8c09477ec0baf7db
Kieron <kieron@mentor.gg>
2020-06-19 09:18:32 +0200

    Add more EquipmentElementExtensions

--- SituationOperator/Helpers/EquipmentElementExtensions.cs
+++ SituationOperator/Helpers/EquipmentElementExtensions.cs
@@ -18,9 +18,32 @@ namespace SituationOperator.Helpers
             EquipmentElement.Decoy
         };
 
+        public static readonly List<EquipmentElement> SniperRifles = new List<EquipmentElement>
+        {
+            EquipmentElement.Scout,
+            EquipmentElement.AWP,
+            EquipmentElement.Scar20,
+            EquipmentElement.G3SG1
+        };
+
         public static bool IsGrenade(this EquipmentElement element)
         {
             return Grenades.Contains(element);
+        }
+
+        public static bool IsGun(this EquipmentElement element)
+        {
+            return 1 <= (short) element && (short) element < 400;
+        }
+
+        public static bool IsRifle(this EquipmentElement element)
+        {
+            return 301 <= (short)element && (short)element < 400;
+        }
+
+        public static bool IsSniperRifle(this EquipmentElement element)
+        {
+            return SniperRifles.Contains(element);
         }
     }
 }
