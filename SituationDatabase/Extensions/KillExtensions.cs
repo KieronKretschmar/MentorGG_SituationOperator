d6a8748e1a22d18b40000f6a7ac59eb15ef35af3
Kieron <kieron@mentor.gg>
2020-06-19 10:03:19 +0200

    Add KillExtensions to simplify code

---
+++ SituationDatabase/Extensions/KillExtensions.cs
@@ -0,0 +1,18 @@
+﻿using MatchEntities;
+using SituationDatabase.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SituationDatabase.Extensions
+{
+    public static class KillExtensions
+    {
+        public static bool ThroughCover(this Kill kill)
+        {
+            return kill.KillType == MatchEntities.Enums.KillType.CoverBodyShot
+                || kill.KillType == MatchEntities.Enums.KillType.CoverHeadShot;
+        }
+    }
+}
