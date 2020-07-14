77eb42328d8de01eb2af47be57a62d1962258b59
Kieron <kieron@mentor.gg>
2020-07-14 12:45:54 +0200

    Add FeedbackController

---
+++ SituationOperator/Controllers/FeedbackController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SituationDatabase;
+using SituationDatabase.Enums;
+using SituationDatabase.Models;
+using SituationOperator.Enums;
+using SituationOperator.Helpers;
+using SituationOperator.Helpers.SubscriptionConfig;
+using SituationOperator.Models;
+
+namespace SituationOperator.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/public/feedback")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly ILogger<FeedbackController> _logger;
+        private readonly SituationContext _context;
+
+        public FeedbackController(
+            ILogger<FeedbackController> logger,
+            SituationContext context)        
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets all SituationTypes' metadata objects.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{steamId}")]
+        public async Task<ActionResult<FeedbackModel>> GetFeedbackAsync(long steamId)
+        {
+            var feedbacks = await _context.UserFeedback
+                .Where(x => x.SteamId == steamId)
+                .ToListAsync();
+
+            var model = new FeedbackModel(feedbacks);
+            return model;
+        }
+
+        [HttpPost("{steamId}")]
+        public async Task<ActionResult> PostFeedbackAsync(long matchId, SituationType situationType, long situationId, long steamId, bool isPositive, string comment)
+        {
+            var entity = await _context.UserFeedback.FindAsync(matchId, situationType, situationId, steamId);
+            if(entity == null)
+            {
+                entity = new UserFeedback(matchId, situationType, situationId, steamId, isPositive, comment);
+                _context.UserFeedback.Add(entity);
+            }
+            else
+            {
+                entity.IsPositive = isPositive;
+                entity.Comment = comment;
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ SituationOperator/Models/FeedbackModel.cs
@@ -0,0 +1,18 @@
+﻿using SituationDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SituationOperator.Models
+{
+    public class FeedbackModel
+    {
+        public List<UserFeedback> UserFeedbacks { get; set; }
+
+        public FeedbackModel(List<UserFeedback> userFeedbacks)
+        {
+            UserFeedbacks = userFeedbacks;
+        }
+    }
+}
